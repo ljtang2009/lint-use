d3328f9e69008a0028ffbb1478f9dedd8f60031f
Tang Lijin <ho_ho_gl@hotmail.com>
2024-04-03 18:26:32 +0800

    style(other): :lipstick: format vitepress config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,27 +1,27 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
 
 export default defineConfig({
-  title: "LintUse",
-  description: "Collection of Lint Utilities",
+  title:       'LintUse',
+  description: 'Collection of Lint Utilities',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Examples', link: '/markdown-examples' },
-      { text: 'TypeDoc', link: '/typedoc/index.html', target: '_blank'}
+      { text: 'TypeDoc', link: '/typedoc/index.html', target: '_blank' },
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text:  'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+  },
+});
