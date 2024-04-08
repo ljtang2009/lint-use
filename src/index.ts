4deefb40f32362ecd9bd2fd504f2383c7f5bb8ba
Tang Lijin <ho_ho_gl@hotmail.com>
2024-04-08 22:47:05 +0800

    feat(src): :sparkles: add prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * as eslint from './eslint/index.js';
 export * as stylelint from './stylelint/index.js';
 export * as markdownlint from './markdownlint/index.js';
+export * as prettier from './prettier/index.js';
